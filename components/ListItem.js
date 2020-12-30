7a0e5ab4625019429506d75c3a2e78316bbccf9f
Alejandro Murawczik <amurawczik@almacorp.es>
2020-12-30 14:11:12 +0100

    ADD files

---
+++ components/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {paddingHorizontal: 15, height: 60, justifyContent: 'center', borderBottomWidth: 1, borderBottomColor: '#eee'},
+    text: {fontSize: 18},
+})
+
+export default ({name, onPress}) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+            <Text style={styles.text}>{name}</Text>
+        </TouchableOpacity>
+    )
+}
